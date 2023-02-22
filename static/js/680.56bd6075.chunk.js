75facd6879e9080452ad3169c53ee4d4ebaa4a16
VitaliiBerko <106317469+VitaliiBerko@users.noreply.github.com>
2023-02-22 14:35:25 +0000

    Deploying to gh-pages from @ VitaliiBerko/goit-react-hw-08-phonebook@e50316c43813ade2d00987f8be9c270c1368ca57 🚀

---
+++ static/js/680.56bd6075.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,o,n){n.r(o);var t=n(1228),s=n(184);o.default=function(){return(0,s.jsx)("div",{className:t.Z.container,children:(0,s.jsx)("h2",{className:t.Z.textCentr,children:"Hello! This is your phonebook!"})})}}}]);
+//# sourceMappingURL=680.56bd6075.chunk.js.map
